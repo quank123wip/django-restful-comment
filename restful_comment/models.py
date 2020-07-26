5a4196a205c7b27a7dd5e193123d0ae8a3213b4a
quank123wip <quank123wip@outlook.com>
2020-07-26 17:17:05 +0800

    feat: Core Model

--- restful_comment/models.py
+++ restful_comment/models.py
@@ -8,24 +8,16 @@ from django_comments.managers import CommentManager
 from django_comments.models import Comment, CommentFlag, CommentAbstractModel
 
 from vote.models import VoteModel
+from mptt.models import MPTTModel, TreeForeignKey
 
-
-class RestfulCommentManager(CommentManager):
-    def get_queryset(self):
-        return super(RestfulCommentManager, self).get_queryset()
 
 class RestfulCommentModel(VoteModel, Comment):
-    object_id = models.UUIDField()
-    father_id = models.UUIDField(null=True)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    content_object = GenericForeignKey()
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
     replies = ArrayField(models.UUIDField())
 
-    def save():
-        pass
 
-    def get_comment_tree():
-        pass
+    class MPTTMeta:
+        order_insertion_by = ['submit_date']
     
     
     
